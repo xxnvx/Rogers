e89146f031a18d549b798fe7bfee83f85a2b5ee4
xxnvx <139485784+xxnvx@users.noreply.github.com>
2023-07-13 09:08:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_bleeahe", "template_3sjzma2", formData)
+    emailjs.send("service_pef53ah", "template_x7gdyqs", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
